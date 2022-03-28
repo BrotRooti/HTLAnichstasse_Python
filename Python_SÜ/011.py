a30aebbd4a559b90b0f03dbbc68747d40ec4074c
Phillip <philscher@tsn.at>
2022-03-28 08:29:22 +0200

    Code Update

--- Python_SÜ/011.py
+++ Python_SÜ/011.py
@@ -4,7 +4,7 @@ Phillip
 28.03.22
 '''
 
-def = spannungsteiler (r1,r2,u_ges):
+def spannungsteiler(r1,r2,u_ges):
     u1 = r1*(u_ges/(r1+r2))
     u2 = r2*(u_ges/(r1+r2))
     return (u1,u2)
@@ -14,3 +14,5 @@ print ("### Wilkommen zum Spannungsteiler rechnen ###")
 r1 = int(input("Bitte R1 in k\u03A9 eingeben ->"))
 r2 = int(input("Bitte R2 in k\u03A9 eingeben ->"))
 u = int(input("Bitte Uges in V eingeben ->"))
+erg = spannungsteiler(r1,r2,u)
+print("U1={}V und U2={}V".format(erg[0],erg[1]))
